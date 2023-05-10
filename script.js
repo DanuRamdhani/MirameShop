be9967fc6ccbcf08f161cdcaf8aa03fde8c38a1f
Danu Ramdhani <danurd.study@gmail.com>
2023-05-10 14:23:07 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,255 @@
+function showImageDescription(imageName) {
+  var description = {
+    'Images/1': {
+      title: 'Masker Kf94 4Ply 4 ply korea Evo Plusmed Convex 4D Hitam Putih 10pcs',
+      desc: 'Masker premium yang bisa digunakan untuk mencegah bakteri dan virus, tidak gatal dimuka tidak membuat alergi pada kulit muka. KF94 mencegah partikel kecil dan radikal bebas masuk ke dalam rongga hidung dan mulut sehingga aman digunakan untuk mobilitas sehari - hari menuju new normal.',
+      harga: 'Rp.27.000',
+    },
+    'Images/2': {
+      title: 'kaos import lengan pendek pria | baju pria import polos kaos - Putih',
+      desc: `
+      note :
+      - Smua Foto di edit dan di Foto langsung oleh owner. ( Tidak ambil Foto dr org lain)
+      - Size , warna , dan bentuk di gambar sesuai dengan keadaan asli .
+      - Silahkan tanya dulu model dan size yang anda inginkan sebelum order ,untuk menghindari kekosongan stock .
+      - Bahan Halus & Tidak kasar
+      `,
+      harga: 'Rp.95.000',
+    },
+    'Images/3': {
+      title: 'kaos import lengan pendek pria | baju pria import polos kaos - Hitam',
+      desc: `
+      note :
+      - Smua Foto di edit dan di Foto langsung oleh owner. ( Tidak ambil Foto dr org lain)
+      - Silahkan tanya dulu model dan size yang anda inginkan sebelum order ,untuk menghindari kekosongan stock .
+      - Size , warna , dan bentuk di gambar sesuai dengan keadaan asli .
+      - Bahan Halus & Tidak kasar
+      `,
+      harga: 'Rp.95.000',
+    },
+    'Images/4': {
+      title: 'Lap MICROFIBER 30x30 Lembut Tebal 300GSM - Paket 5pcs',
+      desc: 'Terbuat dari Microfiber (80% Polyester & 20% Polyamide) yang tebal dan halus untuk PENYERAPAN AIR maupun DETAILING kendaraan kesayangan anda.',
+      harga: 'Rp.35.000',
+    },
+    'Images/5': {
+      title: 'COLOKAN STOP KONTAK LISTRIK 3 LUBANG SAKLAR',
+      desc: `Kualitas bahan sangat baik tidak mudah terbakar
+      Praktis dilengkapi dengan Saklar ON-OFF sehingga tidak perlu mencabut dari colokan listrik.
+      cocok untuk pemakaian alat listrik ringan seperti kulkas,TV, Magic com dll.`,
+      harga: 'Rp.1.000',
+    },
+    'Images/6': {
+      title: 'WAX POLISH NANOTECH PROTECTION | PENGKILAP MOBIL | WAX MOBIL & MOTOR',
+      desc: `Berfungsi sebagai finishing polish atau wax polish untuk kondisi cat yang belum sempurna,
+      masih banyak spiderweb/baret halus, kusam ataupun cat yang sudah mulai memudar.
+      WAX POLISH akan mengkilapkan dan mencerahkan warna kembali.
+      Melindungi cat dari kusam akibat sinar UV, mencegah terjadinya waterspot/jamur body,
+      mengunci kilap lebih dalam untuk memberikan efek glossy dan hydrophobic/anti air.
+      Untuk kondisi cat yang masih bagus/hanya perlu maintenance, gunakan Nanotech PURE PASTE WAX Carnauba.
+      `,
+      harga: 'Rp.70.000',
+    },
+    'Images/7': {
+      title: 'COLOKAN STOP KONTAK LISTRIK 3 LUBANG SAKLAR',
+      desc: `Kualitas bahan sangat baik tidak mudah terbakar
+      Praktis dilengkapi dengan Saklar ON-OFF sehingga tidak perlu mencabut dari colokan listrik.
+      cocok untuk pemakaian alat listrik ringan seperti kulkas,TV, Magic com dll.`,
+      harga: 'Rp.1.000',
+    },
+    'Images/8': {
+      title: 'COLOKAN STOP KONTAK LISTRIK 3 LUBANG SAKLAR',
+      desc: `Kualitas bahan sangat baik tidak mudah terbakar
+      Praktis dilengkapi dengan Saklar ON-OFF sehingga tidak perlu mencabut dari colokan listrik.
+      cocok untuk pemakaian alat listrik ringan seperti kulkas,TV, Magic com dll.`,
+      harga: 'Rp.1.000',
+    },
+    'Images/10': {
+      title: 'COLOKAN STOP KONTAK LISTRIK 3 LUBANG SAKLAR',
+      desc: `Kualitas bahan sangat baik tidak mudah terbakar
+      Praktis dilengkapi dengan Saklar ON-OFF sehingga tidak perlu mencabut dari colokan listrik.
+      cocok untuk pemakaian alat listrik ringan seperti kulkas,TV, Magic com dll.`,
+      harga: 'Rp.1.000',
+    },
+    'Images/11': {
+      title: 'COLOKAN STOP KONTAK LISTRIK 3 LUBANG SAKLAR',
+      desc: `Kualitas bahan sangat baik tidak mudah terbakar
+      Praktis dilengkapi dengan Saklar ON-OFF sehingga tidak perlu mencabut dari colokan listrik.
+      cocok untuk pemakaian alat listrik ringan seperti kulkas,TV, Magic com dll.`,
+      harga: 'Rp.1.000',
+    },
+    'Images/13': {
+      title: 'COLOKAN STOP KONTAK LISTRIK 3 LUBANG SAKLAR',
+      desc: `Kualitas bahan sangat baik tidak mudah terbakar
+      Praktis dilengkapi dengan Saklar ON-OFF sehingga tidak perlu mencabut dari colokan listrik.
+      cocok untuk pemakaian alat listrik ringan seperti kulkas,TV, Magic com dll.`,
+      harga: 'Rp.1.000',
+    },
+    'Images/14': {
+      title: 'COLOKAN STOP KONTAK LISTRIK 3 LUBANG SAKLAR',
+      desc: `Kualitas bahan sangat baik tidak mudah terbakar
+      Praktis dilengkapi dengan Saklar ON-OFF sehingga tidak perlu mencabut dari colokan listrik.
+      cocok untuk pemakaian alat listrik ringan seperti kulkas,TV, Magic com dll.`,
+      harga: 'Rp.1.000',
+    },
+    'Images/15': {
+      title: 'COLOKAN STOP KONTAK LISTRIK 3 LUBANG SAKLAR',
+      desc: `Kualitas bahan sangat baik tidak mudah terbakar
+      Praktis dilengkapi dengan Saklar ON-OFF sehingga tidak perlu mencabut dari colokan listrik.
+      cocok untuk pemakaian alat listrik ringan seperti kulkas,TV, Magic com dll.`,
+      harga: 'Rp.1.000',
+    },
+    'Images/16': {
+      title: 'COLOKAN STOP KONTAK LISTRIK 3 LUBANG SAKLAR',
+      desc: `Kualitas bahan sangat baik tidak mudah terbakar
+      Praktis dilengkapi dengan Saklar ON-OFF sehingga tidak perlu mencabut dari colokan listrik.
+      cocok untuk pemakaian alat listrik ringan seperti kulkas,TV, Magic com dll.`,
+      harga: 'Rp.1.000',
+    },
+    'Images/17': {
+      title: 'COLOKAN STOP KONTAK LISTRIK 3 LUBANG SAKLAR',
+      desc: `Kualitas bahan sangat baik tidak mudah terbakar
+      Praktis dilengkapi dengan Saklar ON-OFF sehingga tidak perlu mencabut dari colokan listrik.
+      cocok untuk pemakaian alat listrik ringan seperti kulkas,TV, Magic com dll.`,
+      harga: 'Rp.1.000',
+    },
+    'Images/18': {
+      title: 'COLOKAN STOP KONTAK LISTRIK 3 LUBANG SAKLAR',
+      desc: `Kualitas bahan sangat baik tidak mudah terbakar
+      Praktis dilengkapi dengan Saklar ON-OFF sehingga tidak perlu mencabut dari colokan listrik.
+      cocok untuk pemakaian alat listrik ringan seperti kulkas,TV, Magic com dll.`,
+      harga: 'Rp.1.000',
+    },
+    'Images/19': {
+      title: 'COLOKAN STOP KONTAK LISTRIK 3 LUBANG SAKLAR',
+      desc: `Kualitas bahan sangat baik tidak mudah terbakar
+      Praktis dilengkapi dengan Saklar ON-OFF sehingga tidak perlu mencabut dari colokan listrik.
+      cocok untuk pemakaian alat listrik ringan seperti kulkas,TV, Magic com dll.`,
+      harga: 'Rp.1.000',
+    },
+    'Images/20': {
+      title: 'COLOKAN STOP KONTAK LISTRIK 3 LUBANG SAKLAR',
+      desc: `Kualitas bahan sangat baik tidak mudah terbakar
+      Praktis dilengkapi dengan Saklar ON-OFF sehingga tidak perlu mencabut dari colokan listrik.
+      cocok untuk pemakaian alat listrik ringan seperti kulkas,TV, Magic com dll.`,
+      harga: 'Rp.1.000',
+    },
+  };
+
+  if (description.hasOwnProperty(imageName)) {
+    var image = description[imageName];
+    var newWindow = window.open('', '_blank');
+
+    newWindow.document.write(`
+    <!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>${image.title}</title>
+    <link rel="stylesheet" type="text/css" href="foot_and_head.css">
+    <link rel="stylesheet" href="hal2.css">
+    <link rel="stylesheet" href="fonts6/css/all.css">
+    <link rel="stylesheet" href="search.css">
+    <link rel="stylesheet" href="fonts.css">
+</head>
+
+<body>
+
+    <header>
+        <div class="top-nav">
+            <div class="left">
+                <a href="#"><i class="fa-brands fa-instagram"></i> Instagram</a>
+                <a class="line">|</a>
+                <a href="#"><i class="fa-brands fa-tiktok"></i> Tiktok</a>
+                <a class="line">|</a>
+                <a href="#"><i class="fa-brands fa-google-play"></i> MirameShop App</a>
+            </div>
+            <div class="right">
+                <a href="#"><i class="fa-solid fa-bag-shopping"></i> Belanja</a>
+                <a class="line">|</a>
+                <a href="#"><i class="fa-solid fa-money-check-dollar"></i> Pembayaran</a>
+                <a class="line">|</a>
+                <a href="#"><i class="fa-solid fa-user"></i> Akun Saya</a>
+            </div>
+        </div>
+        <div class="bottom-nav">
+            <div class="logo"><img src="Images/MS-transformed.png" alt="" height="70px"></div>
+            <div class="search">
+                <div class="search">
+                    <input type="text" class="search__input" placeholder="Cari Produk">
+                    <button class="search__button">
+                        <svg class="search__icon" aria-hidden="true" viewBox="0 0 24 24">
+                            <g>
+                                <path
+                                    d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z">
+                                </path>
+                            </g>
+                        </svg>
+                    </button>
+                </div>
+            </div>
+            <a><i class="fa-solid fa-cart-shopping"></i></a>
+            <a href=""><button class="button masuk">Masuk</button></a>
+            <a href=""><button class="button daftar">Daftar</button></a>
+        </div>
+    </header>
+
+    <main>
+        <div class="image">
+            <img src="${imageName}.jpg" alt="${image.title}" height="350px">
+            <div class="container">
+                <div class="product">
+                    <div class="product-details">
+                        <h2>${image.title}</h2>
+                        <p class="product-price">${image.harga}</p>
+                        <p class="product-description">${image.desc}</p>
+                    </div>
+                </div>
+
+                <div class="buttons">
+                    <button class="add-to-cart">Add to Cart</button>
+                    <button class="buy-now">Buy Now</button>
+                </div>
+            </div>
+        </div>
+    </main>
+
+    <footer>
+        <div class="container">
+            <div class="footer-content">
+                <div class="footer-section about">
+                    <h3>About Us</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sit amet nunc urna. </p>
+                </div>
+                <div class="footer-section links">
+                    <h3>Quick Links</h3>
+                    <ul>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Products</a></li>
+                        <li><a href="#">Categories</a></li>
+                        <li><a href="#">Contact</a></li>
+                    </ul>
+                </div>
+                <div class="footer-section contact">
+                    <h3>Contact Us</h3>
+                    <p>Email: Miramemshop@gmail.com</p>
+                    <p>Phone: 123-456-7890</p>
+                </div>
+            </div>
+        </div>
+        <div class="footer-bottom">
+            <p>&copy; 2023 MirameShop. All rights reserved.</p>
+        </div>
+    </footer>
+
+</body>
+
+</html>
+      `);
+
+    newWindow.document.close();
+  }
+}
